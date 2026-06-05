bddede78dcf42087198d6240f9194ebac0005659
Sarai <sarai@MacBook-Neo-de-Sarai.local>
2026-06-05 13:21:44 -0400

    Exercise lodash 27 completed

--- exercises/ex27.js
+++ exercises/ex27.js
@@ -10,7 +10,15 @@
 const _ = require('lodash');
 const movies = require('../data/movies.json');
 
-const lodashSolution = null;
+const lodashSolution = _.chain(movies)
+.filter(movie => movie.rating >= 8.0)
+.map(movie => ({
+  title: movie.title,
+  genre: movie.genre,
+  rating: movie.rating
+}))
+.orderBy(['rating', 'title'], ['desc', 'asc'])
+.value();
 
 console.log(lodashSolution);
 
